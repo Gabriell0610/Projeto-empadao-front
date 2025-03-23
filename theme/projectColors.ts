c16962cf10e60db8e7450cf3d09f07a6db5e825b
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2025-03-23 19:27:40 -0300

    feat: default config

---
+++ theme/projectColors.ts
@@ -0,0 +1,23 @@
+export const projectColors = {
+  primary: {
+    greenLight: '#247301',
+    greenDark: '#074004',
+    greenFluorescent: '#c6ff00',
+  },
+
+  neutral: {
+    white: '#FFF',
+    offWhite: '#F8F8F8',
+    black: '#000',
+  },
+
+  text: {
+    primary: '#222',
+    secondary: '#666',
+    placeholder: '#989898',
+  },
+
+  accent: {
+    bluePetroil: '#004D40', // Novo tom para dar contraste
+  },
+}
