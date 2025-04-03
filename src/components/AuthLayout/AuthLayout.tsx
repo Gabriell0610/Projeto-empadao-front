c5f33e90c48e959d880956ff36592b9fe5e3aa9d
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2025-04-02 21:57:00 -0300

    feat: styling auth screen

--- src/components/AuthLayout/AuthLayout.tsx
+++ src/components/AuthLayout/AuthLayout.tsx
@@ -19,9 +19,9 @@ export const AuthLayout = ({
   const navigate = useRouter()
   return (
     <main className="flex h-screen flex-col items-center justify-center md:flex-row">
-      <section className="w-full md:w-1/2">
+      <section className="w-full lg:w-1/2">
         <div className="relative mx-auto flex w-full max-w-[500px] flex-col justify-center overflow-auto px-8 py-8 md:max-w-[500px]">
-          <div className="absolute left-[-4px] top-7 hidden sm:block">
+          <div className="fixed left-4 top-12 sm:absolute sm:left-[-4px] sm:top-7">
             <FiChevronLeft
               size={32}
               className="cursor-pointer"
@@ -36,7 +36,7 @@ export const AuthLayout = ({
         </div>
       </section>
 
-      <div className="relative hidden h-screen w-full md:block md:w-1/2">
+      <div className="relative hidden h-screen w-1/2 lg:block">
         <Image
           src={imageUrl || ''}
           alt={altImage}
