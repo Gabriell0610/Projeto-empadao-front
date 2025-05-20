a6f4e4645a24bdbf3a05f46937a7081ae8d20cc2
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2025-05-19 22:31:56 -0300

    feat: refactoring images

--- src/app/newPassword/page.tsx
+++ src/app/newPassword/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { ClientPageNewPassword } from './clientPage'
-import imageNewPassword from '../../../public/image_newPassword_teste.png'
+import imageNewPassword from '../../../public/image_newPassword3.png'
 import { AuthLayout } from '@/components/AuthLayout/AuthLayout'
 export default function NewPasswordPage() {
   return (
@@ -9,7 +9,6 @@ export default function NewPasswordPage() {
     altImage='Uma pessoa confusa e pensando na senha perdida' 
     imageUrl={imageNewPassword} 
     title={'Adicione uma nova senha!'}
-    mode="register"
     >
       <ClientPageNewPassword/>
     </AuthLayout>
