f62c3043f038ebe2058319c05c7730ef44cf190f
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2025-05-21 20:46:59 -0300

    feat: makeing test

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
-// next.config.mjs
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-/** Para obter corretamente o __dirname em ESM */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname, 'src'),
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
