c946f9776d2f8f97171d4d9ac88bfb9ca9750a65
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2025-07-01 19:58:21 -0300

    fix: resolve CI error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { Header } from '@/components/Header/Header';
 import Home from './clientPage';
-import { authOptions } from './api/auth/[...nextauth]/route';
 import { getServerSession } from 'next-auth';
 import { useItems } from '@/hooks/useItems';
 import { ListActiveItemsInterface } from '@/utils/types/items.type';
+import { authOptions } from '@/libs/auth';
 export default async function HomePage() {
   const { listActiveItem } = useItems();
   const session = await getServerSession(authOptions);
