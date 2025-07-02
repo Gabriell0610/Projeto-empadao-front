174b6eb9535c76afa502673dafb3511b69f92109
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2025-07-01 21:01:40 -0300

    fix: resolve CI error

--- src/utils/helpers/url.ts
+++ src/utils/helpers/url.ts
@@ -1,5 +1,5 @@
 export const baseUrl = () => {
-  const url = process.env.NEXT_PUBLIC_API_URL;
+  const url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1338/api';
   if (!url) throw new Error('API base URL não definida!');
   return url;
 };
